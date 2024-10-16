a8c11c366d8f07fa12006c6b783337bda09a6761
karthickag04 <karthickag04@gmail.com>
2024-10-16 15:51:15 +0530

    added session files on access modifier and constructor

---
+++ session_02b/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+namespace session_02b
+{
+
+    class Program
+    {
+        public Program(){
+            Console.WriteLine("Welcome to Gtech under constructor in main class - default constructor");
+        }
+        public Program(int a){
+            Console.WriteLine("constructor with one argument");   
+        }
+        
+        public Program(int a, int b){
+            Console.WriteLine("constructor over loading example");
+        }
+        public Program(string a, int b){
+            Console.WriteLine("constructor over loading example");
+        }
+
+
+
+        public static void Main(String[] args)
+        {
+            // example call to check the constructor working or not
+            Program p=new Program();
+        
+            // example call to check the access modifiers declared in another class
+            gtech gt=new gtech();
+            // Console.WriteLine(gt.trainerlaptopNo);
+        
+            Console.WriteLine(gt.practicelaptopNo);
+
+        }
+
+    }
+
+    class gtech{
+        private string trainerlaptopNo="NO08";
+        public string practicelaptopNo="NO10";
+    }
+
+}
