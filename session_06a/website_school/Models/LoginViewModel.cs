6ca670dded67e6c7f7b80a7f44b9189a2dff1cb5
karthickag04 <karthickag04@gmail.com>
2024-10-29 12:39:56 +0530

    added session files with registeration of the user

---
+++ session_06a/website_school/Models/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace website_school.Models
+{
+    public class LoginViewModel
+    {
+        [Key]
+        public int? Login_id { get; set; }
+
+
+        [Required(ErrorMessage = "Username is required.")]
+        [StringLength(100, ErrorMessage = "Username cannot be longer than 100 characters.")]
+        public string? Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+    }
+}
