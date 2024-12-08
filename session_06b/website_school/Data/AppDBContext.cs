79d905bf444599a83ee239e3e1b027d1db580fd4
karthickag04 <karthickag04@gmail.com>
2024-12-08 10:55:02 +0530

    added session files on logger and checking multiple database tables for login inforamtion

---
+++ session_06b/website_school/Data/AppDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using website_school.Models;
+
+namespace website_school.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<LoginViewModel> emp_login { get; set; }
+        public DbSet<RegisterViewModel> reg_table { get; set; }
+
+    }
+}
